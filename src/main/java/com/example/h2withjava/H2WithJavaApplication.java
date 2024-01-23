d56855ce5996caa4ff4e6fb47ba3e67be2dd2588
Bryan Hansen <bryankhansen@gmail.com>
2024-01-22 19:02:36 -0700

    Same code with Spring Jdbc

--- src/main/java/com/example/h2withjava/H2WithJavaApplication.java
+++ src/main/java/com/example/h2withjava/H2WithJavaApplication.java
@@ -2,323 +2,216 @@ package com.example.h2withjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
+import javax.sql.DataSource;
 import java.sql.*;
+import java.util.List;
 
 @SpringBootApplication
 public class H2WithJavaApplication {
-    private static String JDBC_DRIVER = "org.h2.Driver";
-    private static String DB_URL = "jdbc:h2:mem:testdb";
-    private static String USER = "sa";
-    private static String PASSWORD = "";
 
-    private static Connection conn = null;
-    private static Statement stmt = null;
 
     public static void main(String[] args) {
-        SpringApplication.run(H2WithJavaApplication.class, args);
+        ApplicationContext context = SpringApplication.run(H2WithJavaApplication.class, args);
 
-        //Step 1:  Set up the driver
-        try {
-            Class.forName(JDBC_DRIVER);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+        DataSource dataSource = context.getBean(DataSource.class);
 
-        createRegistrationTable();
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 
-        addDataToRegistrationTable();
+        createRegistrationTable(jdbcTemplate);
 
-        retrieveDataFromRegistrationTable();
+        addDataToRegistrationTable(jdbcTemplate);
 
-        updateAndRetrieveDataFromRegistration();
+        retrieveDataFromRegistrationTable(jdbcTemplate);
 
-        retrieveDataFromEmployees();
+        updateAndRetrieveDataFromRegistration(jdbcTemplate);
 
-        retrieveDataFromEmployeesWithTableJoin();
+        retrieveDataFromEmployees(jdbcTemplate);
+
+        retrieveDataFromEmployeesWithTableJoin(jdbcTemplate);
     }
 
-    private static void createRegistrationTable() {
-        try {
-            //Step 2:  Open a connection to the database
-            conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+    private static void createRegistrationTable(JdbcTemplate jdbcTemplate) {
 
-            //Step 3 Prepare a SQL query
-            stmt = conn.createStatement();
-            String sql = "CREATE TABLE   REGISTRATION " +
-                    "(id INTEGER AUTO_INCREMENT  PRIMARY KEY, " +
-                    " first VARCHAR(255), " +
-                    " last VARCHAR(255), " +
-                    " age INTEGER)";
+        String sql = "CREATE TABLE   REGISTRATION " +
+                "(id INTEGER AUTO_INCREMENT  PRIMARY KEY, " +
+                " first VARCHAR(255), " +
+                " last VARCHAR(255), " +
+                " age INTEGER)";
 
-            //Step 4:  Execute the SQL statement
-            stmt.executeUpdate(sql);
+        jdbcTemplate.execute(sql);
 
-            // STEP 6 and 7: Clean-up environment
-
-            //Step 5:  Handle the response.  We don't have to do this for a CREATE statement
-
-            //Steps 6 and 7:  Close open things
-            stmt.close();
-            conn.close();
-        } catch (
-                SQLException ex) {
-            throw new RuntimeException(ex);
-        } catch (Exception e) {
-            //Handle errors for Class.forName
-            e.printStackTrace();
-        } finally {
-            //finally block used to close resources
-            try {
-                if (stmt != null) stmt.close();
-            } catch (SQLException se2) {
-            } // nothing we can do
-            try {
-                if (conn != null) conn.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
-        }
     }
 
-        public static void addDataToRegistrationTable()  {
-            try {
-                //Step 2:  Open a connection to the database
-                conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        public static void addDataToRegistrationTable(JdbcTemplate jdbcTemplate)  {
 
-                //Step 3 Prepare a SQL query
-                stmt = conn.createStatement();
-                String sql = "insert into REGISTRATION(first, last, age)  VALUES" +
+            String sql = "insert into REGISTRATION(first, last, age)  VALUES" +
                         "('Bill', 'Fairfield', 71)";
 
-                //Step 4:  Execute the SQL statement
-                stmt.executeUpdate(sql);
-
-                // STEP 6 and 7: Clean-up environment
-
-                //Step 5:  Handle the response.  We don't have to do this for a CREATE statement
-
-                //Steps 6 and 7:  Close open things
-                stmt.close();
-                conn.close();
-            } catch (
-                    SQLException ex) {
-                throw new RuntimeException(ex);
-            } catch (Exception e) {
-                //Handle errors for Class.forName
-                e.printStackTrace();
-            } finally {
-                //finally block used to close resources
-                try {
-                    if (stmt != null) stmt.close();
-                } catch (SQLException se2) {
-                } // nothing we can do
-                try {
-                    if (conn != null) conn.close();
-                } catch (SQLException se) {
-                    se.printStackTrace();
-                }
-            }
+            jdbcTemplate.execute(sql);
         }
 
-        public static void retrieveDataFromRegistrationTable()  {
-            try {
-                //Step 2:  Open a connection to the database
-                conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        public static void retrieveDataFromRegistrationTable(JdbcTemplate jdbcTemplate)  {
+            String sql = "SELECT * FROM REGISTRATION";
 
-                //Step 3 Prepare a SQL query
-                stmt = conn.createStatement();
-                String sql = "SELECT * FROM REGISTRATION";
+            List<Person> peeps = jdbcTemplate.query(sql, new RowMapper<Person>() {
+                public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
+                    Person person = new Person();
 
-                //Step 4:  Execute the SQL statement
-                ResultSet rs = stmt.executeQuery(sql);
-
-                //Step 5:  Handle the response.
-                while(rs.next()) {
-                    int id = rs.getInt("id");
-                    String first = rs.getString("first");
-                    String last = rs.getString("last");
-                    int age = rs.getInt("age");
-
-                    System.out.print("ID: " + id);
-                    System.out.print(", First: " + first);
-                    System.out.print(", Last: " + last);
-                    System.out.println(", Age: " + age);
+                    person.setId(rs.getInt("id"));
+                    person.setFirstName(rs.getString("first"));
+                    person.setLastName(rs.getString("last"));
+                    person.setAge(rs.getInt("age"));
+                    return person;
                 }
+            });
 
-                //Steps 6 and 7:  Close open things
-                stmt.close();
-                rs.close();
-                conn.close();
-            } catch (
-                    SQLException ex) {
-                throw new RuntimeException(ex);
-            } catch (Exception e) {
-                //Handle errors for Class.forName
-                e.printStackTrace();
-            } finally {
-                //finally block used to close resources
-                try {
-                    if (stmt != null) stmt.close();
-                } catch (SQLException se2) {
-                } // nothing we can do
-                try {
-                    if (conn != null) conn.close();
-                } catch (SQLException se) {
-                    se.printStackTrace();
-                }
-            }
+            peeps.forEach(System.out::println);
+
         }
 
-    public static void updateAndRetrieveDataFromRegistration() {
-        try {
-            // STEP 2: Open a connection
+    public static void updateAndRetrieveDataFromRegistration(JdbcTemplate jdbcTemplate) {
+        String sql = "UPDATE Registration " +
+                "SET age = 30 WHERE first='Bill'";
 
-            conn = DriverManager.getConnection(DB_URL,USER,PASSWORD);
 
-            // STEP# 3 and 4: Prepare and Execute two queries.  Note that we only need one statement and one sql declaration
-            stmt = conn.createStatement();
-            String sql = "UPDATE Registration " + "SET age = 30 WHERE first='Bill'";
-            stmt.executeUpdate(sql);
+        jdbcTemplate.execute(sql);
 
-            sql = "SELECT id, first, last, age FROM REGISTRATION";
+        sql = "SELECT id, first, last, age FROM REGISTRATION";
 
-            ResultSet rs = stmt.executeQuery(sql);
+        List<Person> peeps = jdbcTemplate.query(sql, new RowMapper<Person>() {
+            public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Person person = new Person();
 
-            // STEP 5: Extract data from result set
-            while(rs.next()) {
-                int id  = rs.getInt("id");
-                String first = rs.getString("first");
-                String last = rs.getString("last");
-                int age = rs.getInt("age");
+                person.setId(rs.getInt("id"));
+                person.setFirstName(rs.getString("first"));
+                person.setLastName(rs.getString("last"));
 
-                System.out.print("ID: " + id);
-                System.out.print(", First: " + first);
-                System.out.print(", Last: " + last);
-                System.out.println(", Age: " + age);
+                return person;
             }
+        });
 
-            // STEP 6 and 7: Clean-up environment
-            rs.close();
-            stmt.close();
-            conn.close();
-        } catch(SQLException se) {
-            // Handle errors for JDBC
-            se.printStackTrace();
-        } catch(Exception e) {
-            // Handle errors for Class.forName
-            e.printStackTrace();
-        } finally {
-            // finally block used to close resources
-            try {
-                if(stmt!=null) stmt.close();
-            } catch(SQLException se2) {
-            } // nothing we can do
-            try {
-                if(conn!=null) conn.close();
-            } catch(SQLException se) {
-                se.printStackTrace();
-            }
-        }
+        peeps.forEach(person -> {
+                System.out.println(person.getId());
+                System.out.println(person.getFirstName());
+                System.out.println(person.getLastName());
+        });
     }
 
-    public static void retrieveDataFromEmployees()  {
-        try {
-            //Step 2:  Open a connection to the database
-            conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+    public static void retrieveDataFromEmployees(JdbcTemplate jdbcTemplate)  {
 
-            //Step 3 Prepare a SQL query
-            stmt = conn.createStatement();
-            String sql = "SELECT * FROM EMPLOYEES";
+        String sql = "SELECT * FROM EMPLOYEES";
 
-            //Step 4:  Execute the SQL statement
-            ResultSet rs = stmt.executeQuery(sql);
+        List<Person> peeps = jdbcTemplate.query(sql, new RowMapper<Person>() {
+            public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Person person = new Person();
 
-            //Step 5:  Handle the response.
-            while(rs.next()) {
-                int id = rs.getInt("employee_id");
-                String first_name = rs.getString("first_name");
-                String last_name = rs.getString("last_name");
-                String email = rs.getString("email");
-                int company_id = rs.getInt("company_id");
+                person.setId(rs.getInt("employee_id"));
+                person.setFirstName(rs.getString("first_name"));
+                person.setLastName(rs.getString("last_name"));
+                person.setEmailAddress(rs.getString("email"));
+                person.setCompanyName(rs.getString("company_id"));
 
-                System.out.print("ID: " + id);
-                System.out.print(", First: " + first_name);
-                System.out.print(", Last: " + last_name);
-                System.out.print(", Email: " + email);
-                System.out.println(", Company ID: " + company_id);
+                return person;
             }
+        });
 
-            //Steps 6 and 7:  Close open things
-            stmt.close();
-            conn.close();
-        } catch (
-                SQLException ex) {
-            throw new RuntimeException(ex);
-        } catch (Exception e) {
-            //Handle errors for Class.forName
-            e.printStackTrace();
-        } finally {
-            //finally block used to close resources
-            try {
-                if (stmt != null) stmt.close();
-            } catch (SQLException se2) {
-            } // nothing we can do
-            try {
-                if (conn != null) conn.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
-        }
+        peeps.forEach(person -> {
+            System.out.println(person.getId());
+            System.out.println(person.getFirstName());
+            System.out.println(person.getLastName());
+            System.out.println(person.getEmailAddress());
+            System.out.println(person.getCompanyName());
+        });
     }
 
-    public static void retrieveDataFromEmployeesWithTableJoin()  {
-        try {
-            //Step 2:  Open a connection to the database
-            conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+    public static void retrieveDataFromEmployeesWithTableJoin(JdbcTemplate jdbcTemplate)  {
 
-            //Step 3 Prepare a SQL query
-            stmt = conn.createStatement();
-            String sql = ("SELECT first_name, last_name, email, company_name FROM EMPLOYEES" +
-                    " JOIN COMPANIES ON COMPANIES.company_id = EMPLOYEES.company_id");
+        String sql = ("SELECT first_name, last_name, email, company_name FROM EMPLOYEES" +
+                " JOIN COMPANIES ON COMPANIES.company_id = EMPLOYEES.company_id");
 
-            //Step 4:  Execute the SQL statement
-            ResultSet rs = stmt.executeQuery(sql);
+        List<Person> peeps = jdbcTemplate.query(sql, new RowMapper<Person>() {
+            public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Person person = new Person();
 
-            //Step 5:  Handle the response.
-            while(rs.next()) {
-                String first_name = rs.getString("first_name");
-                String last_name = rs.getString("last_name");
-                String email = rs.getString("email");
-                String company_name = rs.getString("company_name");
+                person.setFirstName(rs.getString("first_name"));
+                person.setLastName(rs.getString("last_name"));
+                person.setEmailAddress(rs.getString("email"));
+                person.setCompanyName(rs.getString("company_name"));
 
-                System.out.print("First: " + first_name);
-                System.out.print(", Last: " + last_name);
-                System.out.print(", Email: " + email);
-                System.out.println(", Company Name: " + company_name);
+                return person;
             }
+        });
 
-            //Steps 6 and 7:  Close open things
-            stmt.close();
-            conn.close();
-        } catch (
-                SQLException ex) {
-            throw new RuntimeException(ex);
-        } catch (Exception e) {
-            //Handle errors for Class.forName
-            e.printStackTrace();
-        } finally {
-            //finally block used to close resources
-            try {
-                if (stmt != null) stmt.close();
-            } catch (SQLException se2) {
-            } // nothing we can do
-            try {
-                if (conn != null) conn.close();
-            } catch (SQLException se) {
-                se.printStackTrace();
-            }
-        }
+        peeps.forEach(person -> {
+            System.out.println(person.getFirstName());
+            System.out.println(person.getLastName());
+            System.out.println(person.getEmailAddress());
+            System.out.println(person.getCompanyName());
+        });
     }
 }
+class Person {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String companyName;
+
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+
 
